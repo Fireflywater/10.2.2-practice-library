9ebeb2a523050e7549a51f46cfe34c89fbab79d8
James Blakeley <jimmyzb@comcast.net>
2020-03-10 15:18:18 -0700

    initial book view, can build

---
+++ Library.Solution/Library/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Library.Controllers
+{
+  [Authorize]
+  public class BooksController : Controller
+  {
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager; //new line
+
+    public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Book> model = _db.Books.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Book book)
+    {
+      _db.Books.Add(book);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
