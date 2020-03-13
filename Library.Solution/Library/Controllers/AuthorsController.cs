1c69c4f1b25aa1fc5c47cd5ee77125a207a042ea
James Blakeley <jimmyzb@comcast.net>
2020-03-13 14:33:42 -0700

    add author controller and views

---
+++ Library.Solution/Library/Controllers/AuthorsController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Library.Controllers
+{
+  [Authorize]
+  public class AuthorsController : Controller
+  {
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager; //new line
+
+    public AuthorsController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Author> model = _db.Authors.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Author author)
+    {
+      _db.Authors.Add(author);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisAuthor = _db.Authors
+        //.Include(item => item.Categories)
+        //.ThenInclude(join => join.Category)
+        .FirstOrDefault(author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      //ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisAuthor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Author author)
+    {
+      _db.Entry(author).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
