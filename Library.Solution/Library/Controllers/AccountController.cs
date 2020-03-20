8ddb96962f7c1eeb160c7c9878257415437eb668
James Blakeley <jimmyzb@comcast.net>
2020-03-20 11:38:34 -0700

    users can now call themselves librarians on registration

--- Library.Solution/Library/Controllers/AccountController.cs
+++ Library.Solution/Library/Controllers/AccountController.cs
@@ -32,7 +32,7 @@ namespace Library.Controllers
     [HttpPost]
     public async Task<ActionResult> Register (RegisterViewModel model)
     {
-      var user = new ApplicationUser { UserName = model.Email };
+      var user = new ApplicationUser { UserName = model.Email , IsLibrarian = model.IsLibrarian};
       IdentityResult result = await _userManager.CreateAsync(user, model.Password);
       if (result.Succeeded)
       {
--- Library.Solution/Library/ViewModels/RegisterViewModel.cs
+++ Library.Solution/Library/ViewModels/RegisterViewModel.cs
@@ -18,5 +18,7 @@ namespace Library.ViewModels
     [Display(Name = "Confirm password")]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
     public string ConfirmPassword { get; set; }
+
+    public bool IsLibrarian { get; set; }
   }
 }
